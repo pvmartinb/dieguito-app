26824d471c228c0d31f797823650ddd09b6702d5
pvmartinb <pvmartinb@gmail.com>
2022-09-20 21:30:00 -0500

    Se añadio un menu desplegable para indicar la zona siguiente a la que se dirige el paciente, se actualiza el campo zona_siguiente de la tabla usuarios al presionar 'fin'

--- Recepcion.php
+++ Recepcion.php
@@ -12,7 +12,6 @@ include 'cnx.php';
 <body>
 	<form>
 		<?php
-			echo $_POST['inicio'];
 			if (isset($_GET['inicio'])) {
 				$_SESSION['expediente']=$_GET['expediente'];
 				$res=mysqli_query($conectar,"BEGIN WORK");
@@ -36,9 +35,19 @@ include 'cnx.php';
 				$res=mysqli_query($conectar,$sql);
 				$res=mysqli_query($conectar,"COMMIT");
 				$_SESSION['ID']=$max;
-				echo "<br>".$sql;
 				echo "expediente: ".$_SESSION['expediente']."<br>";
 				echo '<form method=POST>';
+				echo 'zona siguiente: <select name=zonaSig>';
+				$sql="select * from zonas";
+				$res=mysqli_query($conectar,$sql);
+				$row=1;
+				$select=NULL;
+				while ($row) {
+					$row=mysqli_fetch_array($res);
+					$select.="<option value=".$row['ID'].">".$row['zona']."</option>";
+				}
+				echo $select;
+				echo "</select><br>";
 				echo '<input type="submit" name="fin" value="fin"></input>';
 				echo '</form>';
 			}
@@ -48,6 +57,9 @@ include 'cnx.php';
 				$sql="update tiempos set fin=";
 				$sql.="'".date('H:i:s')."' ";
 				$sql.="where ID=".$_SESSION['ID'];
+				$res=mysqli_query($conectar,$sql);
+				$sql="update usuarios set zona_siguiente=".$_GET['zonaSig']." where expediente=".$_SESSION['expediente'];
+				#echo $sql;
 				$res=mysqli_query($conectar,$sql);
 				$res=mysqli_query($conectar,"COMMIT");
 				echo '<form method=POST>';
